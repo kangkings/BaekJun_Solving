11df1c33d95ed75992e4b1d428f26236bf25f4c6
kangkings <dlflq11@gmail.com>
2023-07-15 18:43:29 +0900

    Step4.Prob4 Complete

--- src/Step4/Problem3/Prob3.java
+++ src/Step4/Problem3/Prob3.java
@@ -4,23 +4,19 @@ import java.util.Scanner;
 
 public class Prob3 {
     public static void main(String[] args) {
-        /*
-        마찬가지로 배열의 크기를 1_000_000으로 한 점이 걸림.
-         */
+
         Scanner sc = new Scanner(System.in);
-        int [] arr_n = new int[1000000];
-        int n = sc.nextInt();
-        int min = 0, max = 0;
-        for (int i = 0; i < n; i++) {
+        int [] arr_n = new int[9];
+        int max = 0;
+        int num = 0;
+        for (int i = 0; i < arr_n.length; i++) {
             arr_n[i] = sc.nextInt();
-            if(i == 0){
-                min = arr_n[i];
+            if (arr_n[i]>max){
                 max = arr_n[i];
-            }else {
-                if (arr_n[i]<min) min = arr_n[i];
-                else if (arr_n[i]>max)  max=arr_n[i];
+                num = (i+1);
             }
         }
-        System.out.printf("%d %d",min,max);
+        System.out.println(max);
+        System.out.println(num);
     }
 }
